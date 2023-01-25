616dd64618896fa57e83cfa375c1b4b553e9c2e9
KaterinaPy35 <yakushka1308@mail.ru>
2023-01-25 11:29:15 +0300

    Функция для определения четное число или нечетное. Использовали lambda функцию. Использовали декоратор для контроля типа данных.

---
+++ Lesson7/lambda_func.py
@@ -0,0 +1,15 @@
+from beartype import beartype
+
+
+@beartype
+def even_numbers(num: int):
+    '''Функция принимает целое число и печатает информацию: четное или нечетное это число.'''
+    result = lambda num: num % 2
+    if result(num) == 0:
+        print('Число четное')
+    else:
+        print('Число нечетное')
+
+n = int(input('Введите целое число: '))
+
+even_numbers(n)
