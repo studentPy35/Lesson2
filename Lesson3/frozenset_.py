4623d36badcfcb1bd56d0bb427bd87fc119b7eae
KaterinaPy35 <yakushka1308@mail.ru>
2023-02-08 12:20:54 +0300

    Изменения по flake8

--- Lesson3/frozenset_.py
+++ Lesson3/frozenset_.py
@@ -1,12 +1,11 @@
-# создаем frozenset и печатаем их
 myset1 = frozenset([1, 1, 2, 3, 4, 4, 4, 5, 6, 6])
 myset2 = frozenset('aabcccddee')
 
 print(myset1)
 print(myset2)
 
-# объединяем два frozenset
+
 print(myset2.union(myset1))
 
-# находим пересечение множеств
+
 print(myset2.intersection(myset1))
--- Lesson3/int_str_bool_float.py
+++ Lesson3/int_str_bool_float.py
@@ -1,4 +1,3 @@
-# находим сумму, разность, произведение, частное, остаток от деления и т.д. двух целых чисел x, y
 x = int(input('Введите число х: '))
 y = int(input('Введите число y: '))
 addition = x + y
@@ -16,7 +15,6 @@ print('Целочисленное деление х на y:', integer_division)
 print()
 
 
-# операции над строками
 text = 'TeachMeSkills'
 print(text[::-1])
 print(text[1])
@@ -25,7 +23,7 @@ print(text.index('Me'))
 print(text.isalpha())
 print(text.upper())
 
-# операции с bool
+
 print(bool(1))
 print(bool([]))
 print(bool(None))
@@ -33,21 +31,10 @@ print(bool({1, 2, 3}))
 print(bool({}))
 print(bool(tuple()))
 
-# операции с float
+
 x = 2
 print(float(x))
 y = 2.123456789
 print(round(float(y), 4))
 print(2.1 + 2.5)
 print(2.5 / 2)
-
-
-
-
-
-
-
-
-
-
-
