5e4078b0bb96a590e199c3e9abf196741a56d9f4
KaterinaPy35 <yakushka1308@mail.ru>
2023-02-12 21:41:03 +0300

    Added english comments

--- Lesson9/task_4.py
+++ Lesson9/task_4.py
@@ -1,16 +1,18 @@
 from math import sqrt
 
 
-def get_length(point1: tuple, point2: tuple) -> int:
-    """Функция принимает 2 точки(кортежа), возвращает  длину отрезка"""
-    result = sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
-    return result
+def get_length(point1: tuple, point2: tuple) -> float:
+    """The function takes 2 points (tuples), returns
+    the length of the segment
+    """
+    return sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
 
 
-def get_common_length(*args: tuple) -> int:
-    """Функция принимает n-ое количество кортежей (точек координат),
-     состоящих из двух объектов типа int, и возвращает длину пути
-     между этими координатами
+def get_common_length(*args: tuple) -> float:
+    """The function takes the n numbers of tuples
+    (coordinate points), consisting of two int objects,
+    and returns the length of the path between these
+    coordinates
     """
     length = 0
     for index in range(len(args) - 1):
