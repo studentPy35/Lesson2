cf253ab33c15054fe528c547a9119ac9e4f60405
KaterinaPy35 <yakushka1308@mail.ru>
2023-02-13 11:07:03 +0300

    corrected names of arguments, function 'get_information', 'find_info', 'get_companies_by_sector'

--- Lesson9/cache_decorator.py
+++ Lesson9/cache_decorator.py
@@ -1,8 +1,7 @@
 import csv
 
 
-def find_info(dict_list: list) -> list:
-    name = input('Enter the name of company: ')
+def find_info(dict_list: list, name: str) -> list:
     company_list = []
     for company_dict in dict_list:
         if name.lower() in company_dict['Name'].lower():
@@ -10,8 +9,7 @@ def find_info(dict_list: list) -> list:
     return company_list
 
 
-def get_companies_by_sector(dict_list: list) -> list:
-    sector = input('Enter the name of Sector: ')
+def get_companies_by_sector(dict_list: list, sector: str) -> list:
     return list(filter(lambda comp:
                        sector.lower() == comp['Sector'].lower(), dict_list))
 
@@ -24,10 +22,9 @@ def calculate_average_price(dict_list: list) -> float:
 
 
 def get_top_10_companies(dict_list: list) -> list:
-    sorted_dict_list = sorted(dict_list,
-                              key=lambda x: float(x['Price']), reverse=True)
-    return [(company_dict['Name'], company_dict['Price'])
-            for company_dict in sorted_dict_list[:10]]
+    sorted_lst = sorted(dict_list,
+                        key=lambda dct: float(dct['Price']), reverse=True)
+    return [(dct['Name'], dct['Price']) for dct in sorted_lst[:10]]
 
 
 def print_goodbye():
@@ -40,38 +37,37 @@ print('Choose the action from menu:\n1 - Find info by name\n'
 
 
 def use_cache(func):
-    """Decorator for using cache
-    """
+    """Decorator for using cache"""
     cache_dict = {}
 
     def wrapper(*args):
-        nonlocal cache_dict
-        if not cache_dict.get(args):
-            result = func(*args)
-            cache_dict[args] = result
-            return result
-        else:
-            return cache_dict[args]
-        # if args not in cache_dict:
-        #     cache_dict[args] = func(*args)
-        # return cache_dict
+        if args not in cache_dict:
+            cache_dict[args] = func(*args)
+        return cache_dict[args]
+
     return wrapper
 
 
 @use_cache
-def get_information(choice_number: str):
+def get_information(choice: str):
     programm = {1: find_info,
                 2: get_companies_by_sector,
                 3: calculate_average_price,
                 4: get_top_10_companies,
                 5: print_goodbye}
     with open('sp500.csv', encoding='utf8') as file:
-        dict_list_of_companies = list(csv.DictReader(file))
+        company_list = list(csv.DictReader(file))
         try:
-            if int(choice_number) in range(1, 5):
-                return programm[int(choice_number)](dict_list_of_companies)
+            if int(choice) in range(3, 5):
+                return programm[int(choice)](company_list)
+            elif int(choice) == 1:
+                name = input('Enter the name of company: ')
+                return programm[int(choice)](company_list, name)
+            elif int(choice) == 2:
+                sector = input('Enter the name of Sector: ')
+                return programm[int(choice)](company_list, sector)
             else:
-                return programm[int(choice_number)]()
+                return programm[int(choice)]()
         except KeyError:
             return 'You have entered a non-existent ' \
                    'number in the menu. Try again.'
@@ -79,4 +75,6 @@ def get_information(choice_number: str):
             return 'You have to enter a number from menu. Try again.'
 
 
+print(get_information(input('Your choice: ')))
+print(get_information(input('Your choice: ')))
 print(get_information(input('Your choice: ')))
--- Lesson9/console_programm.py
+++ Lesson9/console_programm.py
@@ -2,8 +2,7 @@ import csv
 from functools import lru_cache
 
 
-def find_info(dict_list: list) -> list:
-    name = input('Enter the name of company: ')
+def find_info(dict_list: list, name: str) -> list:
     company_list = []
     for company_dict in dict_list:
         if name.lower() in company_dict['Name'].lower():
@@ -11,15 +10,9 @@ def find_info(dict_list: list) -> list:
     return company_list
 
 
-def get_companies_by_sector(dict_list: list) -> list:
-    sector = input('Enter the name of Sector: ')
-# return list(filter(lambda comp: sector.lower() ==
-    # comp['Sector'].lower(), dict_list))
-    company_list = []
-    for company_dict in dict_list:
-        if sector.lower() == company_dict['Sector'].lower():
-            company_list.append(company_dict['Name'])
-    return company_list
+def get_companies_by_sector(dict_list: list, sector: str) -> list:
+    return list(filter(lambda comp:
+                       sector.lower() == comp['Sector'].lower(), dict_list))
 
 
 def calculate_average_price(dict_list: list) -> float:
@@ -29,11 +22,9 @@ def calculate_average_price(dict_list: list) -> float:
     return round(total_price / len(dict_list), 4)
 
 
-def get_top_10_companies(dict_list: list) -> list:
-    sorted_dict_list = sorted(dict_list,
-                              key=lambda x: float(x['Price']), reverse=True)
-    return [(company_dict['Name'], company_dict['Price'])
-            for company_dict in sorted_dict_list[:10]]
+def get_top_10_companies(lst: list) -> list:
+    sort_lst = sorted(lst, key=lambda x: float(x['Price']), reverse=True)
+    return [(dct['Name'], dct['Price']) for dct in sort_lst[:10]]
 
 
 def print_goodbye():
@@ -46,24 +37,31 @@ print('Choose the action from menu:\n1 - Find info by name\n'
 
 
 @lru_cache
-def get_information(choice_number: str):
+def get_information(choice: str):
     programm = {1: find_info,
                 2: get_companies_by_sector,
                 3: calculate_average_price,
                 4: get_top_10_companies,
                 5: print_goodbye}
     with open('sp500.csv', encoding='utf8') as file:
-        dict_list_of_companies = list(csv.DictReader(file))
+        company_list = list(csv.DictReader(file))
         try:
-            if int(choice_number) in range(1, 5):
-                print(programm[int(choice_number)](dict_list_of_companies))
+            if int(choice) in range(3, 5):
+                return programm[int(choice)](company_list)
+            elif int(choice) == 1:
+                name = input('Enter the name of company: ')
+                return programm[int(choice)](company_list, name)
+            elif int(choice) == 2:
+                sector = input('Enter the name of Sector: ')
+                return programm[int(choice)](company_list, sector)
             else:
-                print(programm[int(choice_number)]())
+                return programm[int(choice)]()
         except KeyError:
-            print('You have entered a non-existent '
-                  'number in the menu. Try again.')
+            return 'You have entered a non-existent ' \
+                   'number in the menu. Try again.'
         except ValueError:
-            print('You have to enter a number from menu. Try again.')
+            return 'You have to enter a number from menu. Try again.'
 
 
-get_information(input('Your choice: '))
+print(get_information(input('Your choice: ')))
+print(get_information(input('Your choice: ')))
