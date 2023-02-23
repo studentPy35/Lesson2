7af1815b824e414e58256bb2b6801db599adb3d4
KaterinaPy35 <yakushka1308@mail.ru>
2023-02-23 14:19:21 +0300

    Сreated a Vector class, which takes two values (tuples). Implemented the method length(), which returns the length of the vector. The class are supported comparison operations between objects of this class

---
+++ Lesson12/task_1.py
@@ -0,0 +1,32 @@
+class Vector:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def length(self):
+        return (((self.point2[0] - self.point1[0]) ** 2 +
+                 (self.point2[1] - self.point1[1]) ** 2) ** 0.5)
+
+    def __lt__(self, other):
+        return self.length() < other.length()
+
+    def __gt__(self, other):
+        return self.length() > other.length()
+
+    def __eq__(self, other):
+        return self.length() == other.length()
+
+    def __ne__(self, other):
+        return self.length() != other.length()
+
+    def __le__(self, other):
+        return self.length() <= other.length()
+
+    def __ge__(self, other):
+        return self.length() >= other.length()
+
+
+vector1 = Vector((1, 1), (1, 10))
+
+vector2 = Vector((1, 5), (1, 1))
+print(vector1 > vector2)
